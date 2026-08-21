34fc5dcb565a67fd2bb0a8df5ef37d31df442877
Nguyen06-CSE <nguyen.cse06@gmail.com>
2026-08-21 21:04:15 +0700

    add ignore

--- QuanLyKhachHang/obj/Debug/net8.0/QuanLyKhachHang.AssemblyInfo.cs
+++ QuanLyKhachHang/obj/Debug/net8.0/QuanLyKhachHang.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuanLyKhachHang")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80980de41e675342a7e7ae5a33e28d31a13a87ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37bc2f16e5de833aef9327cb60bde242b966d6b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuanLyKhachHang")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuanLyKhachHang")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
