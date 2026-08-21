8a24f0058654837704f44bda8f2150f27eceb38b
Nguyen06-CSE <nguyen.cse06@gmail.com>
2026-08-21 12:23:02 +0700

    dashboard trong trang chủ

--- QuanLyKhachHang/App.cs
+++ QuanLyKhachHang/App.cs
@@ -1,3 +1,4 @@
+
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml.Styling;
@@ -28,5 +29,6 @@ namespace QuanLyKhachHang
             }
             base.OnFrameworkInitializationCompleted();
         }
+       
     }
 }
--- QuanLyKhachHang/obj/Debug/net8.0/QuanLyKhachHang.AssemblyInfo.cs
+++ QuanLyKhachHang/obj/Debug/net8.0/QuanLyKhachHang.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuanLyKhachHang")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+639c0b257a2eeba231dccf6c5eb3e0b05d444281")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5e7275334bad13f9a154866ab94af7ee0389c8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuanLyKhachHang")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuanLyKhachHang")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
